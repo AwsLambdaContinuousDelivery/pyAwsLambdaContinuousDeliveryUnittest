7cdf6ebcce32a65b3fa28dcc79107f35acc51b40
Janos Potecki <janos.potecki.15@ucl.ac.uk>
2018-01-18 15:49:56 +0000

    Add `setup.py`

---
+++ setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup( name=' pyAwsLambdaContinuousDeliveryUnittest'
+     , version = '0.0.1'
+     , description = 'pyAwsLambdaContinuousDeliveryUnittest'
+     , author = 'Janos Potecki'
+     , url = 'https://github.com/AwsLambdaContinuousDelivery/pyAwsLambdaContinuousDeliveryUnittest'
+     , packages = ['awslambdacontinuousdelivery.python.test.unittest']
+     , license='MIT'
+     , install_requires = [ 
+          'troposphere'
+        , 'awacs'
+        ]
+     )
